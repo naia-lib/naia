646c73aa288e3146161d0b4b4e419172c6ecf7c9
naia-rs <naia.rust.crate@gmail.com>
2020-05-25 13:19:57 -0600

    get client socket hooked up

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,10 @@
 
 use log::{info};
 
-use gaia_client::GaiaClient;
+use gaia_client::{GaiaClient, ClientEvent};
+
+const PING_MSG: &str = "ping";
+const PONG_MSG: &str = "pong";
 
 pub struct App {
     client: GaiaClient,
@@ -9,22 +12,47 @@ pub struct App {
 }
 
 impl App {
-    pub fn new() -> App {
-        let mut app = App {
-            client: GaiaClient::new(),
-            count: 0,
-        };
+    pub fn new(server_socket_address: &str) -> App {
 
         info!("App Start");
 
-        app
+        App {
+            client: GaiaClient::connect(&server_socket_address),
+            count: 0,
+        }
     }
 
     pub fn update(&mut self) {
-        if self.count > 10 {
-            return;
+
+        match self.client.receive() {
+            Ok(event) => {
+                match event {
+                    ClientEvent::Connection => {
+                        info!("Client connected to: {}", self.client.server_address());
+                        self.client.send(PING_MSG.to_string())
+                            .expect("send error");
+                    }
+                    ClientEvent::Disconnection => {
+                        info!("Client disconnected from: {}", self.client.server_address());
+                    }
+                    ClientEvent::Message(message) => {
+                        info!("Client recv: {}", message);
+
+                        if message.eq(&PONG_MSG.to_string()) && self.count < 10 {
+                            self.count += 1;
+                            let to_server_message: String = PING_MSG.to_string();
+                            info!("Client send: {}", to_server_message);
+                            self.client.send(to_server_message);
+                        }
+                    }
+                    ClientEvent::None => {
+                        //info!("Client non-event");
+                    }
+                }
+            }
+            Err(err) => {
+                info!("Client Error: {}", err);
+            }
         }
-        self.count += 1;
-        info!("App Update {}", self.count);
     }
 }
