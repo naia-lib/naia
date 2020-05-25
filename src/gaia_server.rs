bd28b056c77db167a733c2c7d8037f291f269fe5
naia-rs <naia.rust.crate@gmail.com>
2020-05-25 14:12:47 -0600

    - exposing get_clients() method from socket - introducing artificial drop rate for testing

--- src/gaia_server.rs
+++ src/gaia_server.rs
@@ -4,6 +4,8 @@ use std::{
     error::Error,
 };
 
+use log::info;
+
 use gaia_server_socket::{ServerSocket, SocketEvent, MessageSender, Config};
 
 use super::server_event::ServerEvent;
@@ -12,6 +14,7 @@ use crate::error::GaiaServerError;
 pub struct GaiaServer {
     socket: ServerSocket,
     sender: MessageSender,
+    drop_counter: u8,
 }
 
 impl GaiaServer {
@@ -24,34 +27,48 @@ impl GaiaServer {
         GaiaServer {
             socket: server_socket,
             sender,
+            drop_counter: 0,
         }
     }
 
     pub async fn receive(&mut self) -> Result<ServerEvent, GaiaServerError> {
-        match self.socket.receive().await {
-            Ok(event) => {
-                match event {
-                    SocketEvent::Connection(address) => {
-                        Ok(ServerEvent::Connection(address))
-                    }
-                    SocketEvent::Disconnection(address) => {
-                        Ok(ServerEvent::Disconnection(address))
-                    }
-                    SocketEvent::Message(address, message) => {
-                        Ok(ServerEvent::Message(address, message))
-                    }
-                    SocketEvent::Tick => {
-                        Ok(ServerEvent::Tick)
+        let mut output: Option<Result<ServerEvent, GaiaServerError>> = None;
+        while output.is_none() {
+            match self.socket.receive().await {
+                Ok(event) => {
+                    match event {
+                        SocketEvent::Connection(address) => {
+                            output = Some(Ok(ServerEvent::Connection(address)));
+                        }
+                        SocketEvent::Disconnection(address) => {
+                            output = Some(Ok(ServerEvent::Disconnection(address)));
+                        }
+                        SocketEvent::Message(address, message) => {
+                            if self.drop_counter > 3 {
+                                self.drop_counter = 0;
+                            } else {
+                                self.drop_counter += 1;
+                                output = Some(Ok(ServerEvent::Message(address, message)));
+                            }
+                        }
+                        SocketEvent::Tick => {
+                            output = Some(Ok(ServerEvent::Tick));
+                        }
                     }
                 }
-            }
-            Err(error) => {
-                Err(GaiaServerError::Wrapped(Box::new(error)))
+                Err(error) => {
+                    output = Some(Err(GaiaServerError::Wrapped(Box::new(error))));
+                }
             }
         }
+        return output.unwrap();
     }
 
     pub async fn send(&mut self, message: (SocketAddr, String)) -> Result<(), Box<dyn Error + Send>> {
         return self.sender.send(message).await;
+    }
+
+    pub fn get_clients(&mut self) -> Vec<SocketAddr> {
+        self.socket.get_clients()
     }
 }
