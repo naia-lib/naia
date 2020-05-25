fdc0742c27f3c9e92963b3f47c333fb2a34573ce
naia-rs <naia.rust.crate@gmail.com>
2020-05-25 13:21:38 -0600

    scaffolding set up

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
 
-mod gaia_client;
+pub use gaia_shared::{find_my_ip_address};
 
+mod error;
+
+mod gaia_client;
 pub use gaia_client::GaiaClient;
+
+mod client_event;
+pub use client_event::ClientEvent;
