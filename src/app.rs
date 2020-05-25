4a558b02afec1227fd370fb2f6429e2a4283f8f2
naia-rs <naia.rust.crate@gmail.com>
2020-05-24 21:16:41 -0600

    can test whether app works

--- src/app.rs
+++ src/app.rs
@@ -3,23 +3,28 @@ use log::{info};
 
 use gaia_client::GaiaClient;
 
-const PING_MSG: &str = "ping";
-const PONG_MSG: &str = "pong";
-
 pub struct App {
     client: GaiaClient,
+    count: u8,
 }
 
 impl App {
     pub fn new() -> App {
         let mut app = App {
             client: GaiaClient::new(),
+            count: 0,
         };
+
+        info!("App Start");
 
         app
     }
 
     pub fn update(&mut self) {
-        //
+        if self.count > 10 {
+            return;
+        }
+        self.count += 1;
+        info!("App Update {}", self.count);
     }
 }
