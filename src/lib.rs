a9ba444c17640ffa628ef49a7f82ab05c27caad3
Connor Carpenter <connorcarpenter@gmail.com>
2020-05-25 13:19:57 -0600

    get client socket hooked up

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,9 @@ cfg_if! {
 
         use crate::app::App;
 
+        const SERVER_IP_ADDRESS: &str = "192.168.1.6"; // Put your Server's IP Address here!, can't easily find this automatically from the browser
+        const SERVER_PORT: &str = "3179";
+
         #[wasm_bindgen(start)]
         pub fn main_js() {
             // Uncomment the line below to enable logging. You don't need it if something else (e.g. quicksilver) is logging for you
@@ -20,7 +23,9 @@ cfg_if! {
 
             info!("Gaia Client Example Started");
 
-            let app = App::new();
+            let server_socket_address = SERVER_IP_ADDRESS.to_owned() + ":" + SERVER_PORT;
+
+            let app = App::new(&server_socket_address);
 
             loop_wasm::start_loop(app);
         }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ mod app;
 mod loop_native;
 
 use log::info;
+
+use gaia_client::{find_my_ip_address};
 use app::App;
+
+const SERVER_PORT: &str = "3179";
 
 fn main() {
     // Uncomment the line below to enable logging. You don't need it if something else (e.g. quicksilver) is logging for you
@@ -13,7 +17,9 @@ fn main() {
 
     info!("Gaia Client Example Started");
 
-    let mut app = App::new();
+    let server_socket_address = find_my_ip_address::get() + ":" + SERVER_PORT;
+
+    let mut app = App::new(&server_socket_address);
 
     loop_native::start_loop(&mut app);
 }
