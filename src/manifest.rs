28cbfe602e0acb3a5dd3bad8a7b3593771d753f0
naia-rs <naia.rust.crate@gmail.com>
2020-05-31 13:36:33 -0600

    solidifying NetType, ugly but compiling

--- src/manifest.rs
+++ src/manifest.rs
@@ -1,24 +1,23 @@
 
-//use std::any::{Any, TypeId};
-
-//use anymap::AnyMap;
-
-use crate::NetEvent;
+use std::collections::HashMap;
+use crate::{NetTypeTrait, NetEvent};
 
 pub struct Manifest {
-    //typemap: AnyMap,
+    type_count: u32,
+    type_map: HashMap<u32, Box<dyn NetTypeTrait>>
 }
 
 impl Manifest {
     pub fn new() -> Self {
-        //let mut typemap = AnyMap::new();
         Manifest {
-            //typemap,
+            type_count: 0,
+            type_map: HashMap::new()
         }
     }
 
-    pub fn register_event<T: NetEvent>(&mut self) {
-        //self.typemap.insert(event);
+    pub fn register_type(&mut self, net_type_boxed: Box<dyn NetTypeTrait>) {
+        self.type_map.insert(self.type_count, net_type_boxed);
+        self.type_count += 1;
     }
 
     pub fn process(&mut self) {
