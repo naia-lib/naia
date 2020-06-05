811b89c5362536eb8d5cf1a2824db42daf15b540
naia-rs <naia.rust.crate@gmail.com>
2020-06-05 14:50:13 -0600

    change client/server to take separate event/entity types

--- src/client_event.rs
+++ src/client_event.rs
@@ -3,8 +3,8 @@ pub enum ClientEvent<T> {
     Connection,
     Disconnection,
     Event(T),
-    CreateEntity,
-    UpdateEntity,
-    DeletEntity,
+    CreateEntity(u16),
+    UpdateEntity(u16),
+    DeleteEntity(u16),
     None,
 }
