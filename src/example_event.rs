6b038b8207fc07be981ed6aaa3e117babd2e4109
naia-rs <naia.rust.crate@gmail.com>
2020-06-06 18:27:43 -0600

    remove unneccessary impl for Clone

--- src/example_event.rs
+++ src/example_event.rs
@@ -3,20 +3,13 @@ use gaia_shared::{EventType, NetEvent};
 
 use crate::{StringEvent};
 
+#[derive(Clone)]
 pub enum ExampleEvent {
     StringEvent(StringEvent),
 }
 
 impl EventType for ExampleEvent {
-    fn optional_clone(&self) -> Option<Self> where Self : Sized {
-        match self {
-            ExampleEvent::StringEvent(identity) => {
-                return Some(ExampleEvent::StringEvent(identity.clone()));
-            }
-        }
-    }
-
-    fn use_bytes(&mut self, bytes: &[u8]) {
+    fn read(&mut self, bytes: &[u8]) {
         match self {
             ExampleEvent::StringEvent(identity) => {
                 identity.read(bytes);
