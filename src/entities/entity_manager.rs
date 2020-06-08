79c6d38620fc293d93dd6cc3ad317d4fbc88e39b
Connor Carpenter <connorcarpenter@gmail.com>
2020-06-08 13:04:17 -0600

    server entity manager is able to send messages through to the client

--- src/entities/entity_manager.rs
+++ src/entities/entity_manager.rs
@@ -1,3 +1,5 @@
+use byteorder::{BigEndian, WriteBytesExt};
+
 use super::{
     entity_type::EntityType,
     server_entity_manager::ServerEntityManager,
@@ -6,7 +8,17 @@ use super::{
 
 use slotmap::{new_key_type};
 
-new_key_type! { pub struct LocalEntityKey; }
+pub type LocalEntityKey = u16;
+
+pub trait LocalEntityKeyIO {
+    fn write(&self, out_bytes: &mut Vec<u8>);
+}
+
+impl LocalEntityKeyIO for &LocalEntityKey {
+    fn write(&self, out_bytes: &mut Vec<u8>) {
+        out_bytes.write_u16::<BigEndian>(**self);
+    }
+}
 
 pub enum EntityManager<U: EntityType> {
     Server(ServerEntityManager<U>),
