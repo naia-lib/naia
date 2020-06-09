000bfc08df90176660c15716e3669aa529ebd576
naia-rs <naia.rust.crate@gmail.com>
2020-06-09 00:30:01 -0600

    implement partial read & write

--- src/example_entity.rs
+++ src/example_entity.rs
@@ -1,18 +1,31 @@
 
-use gaia_shared::{EntityType, NetEntity};
+use std::{
+    rc::Rc,
+    cell::RefCell,
+};
+
+use gaia_shared::{EntityType, NetEntity, StateMask};
 
 use crate::{PointEntity};
 
 #[derive(Clone)]
 pub enum ExampleEntity {
-    PointEntity(PointEntity),
+    PointEntity(Rc<RefCell<PointEntity>>),
 }
 
 impl EntityType for ExampleEntity {
     fn read(&mut self, bytes: &[u8]) {
         match self {
             ExampleEntity::PointEntity(identity) => {
-                identity.read(bytes);
+                identity.as_ref().borrow_mut().read(bytes);
+            }
+        }
+    }
+
+    fn read_partial(&mut self, state_mask: &StateMask, bytes: &[u8]) {
+        match self {
+            ExampleEntity::PointEntity(identity) => {
+                identity.as_ref().borrow_mut().read_partial(state_mask, bytes);
             }
         }
     }
