36ad79a635dd747e021f3b7edea0c55de4be0499
Connor Carpenter <connorcarpenter@gmail.com>
2020-06-12 16:15:45 -0600

    pulling out server_connection from gaia_shared

--- src/server_connection.rs
+++ src/server_connection.rs
@@ -5,16 +5,11 @@ use std::{
 };
 
 use gaia_shared::{Timer, PacketType, NetEvent, EventManifest, ClientEntityManager,
-            EventManager, EntityManager, EntityManifest, PacketWriter, PacketReader, ManagerType, HostType,
-            EventType, EntityType, ClientEntityMessage, LocalEntityKey, AckManager, Timestamp, SequenceNumber};
+            EventManager, EntityManifest, PacketWriter, PacketReader, ManagerType, HostType, ServerEntityManager,
+            EventType, EntityType, ClientEntityMessage, LocalEntityKey, AckManager, Timestamp, SequenceNumber, Connection};
 
 pub struct ServerConnection<T: EventType, U: EntityType> {
-    pub connection_timestamp: Timestamp,
-    address: SocketAddr,
-    heartbeat_manager: Timer,
-    timeout_manager: Timer,
-    ack_manager: AckManager,
-    event_manager: EventManager<T>,
+    connection: Connection<T>,
     entity_manager: ClientEntityManager<U>,
 }
 
@@ -22,57 +17,27 @@ impl<T: EventType, U: EntityType> ServerConnection<T, U> {
     pub fn new(address: SocketAddr, heartbeat_interval: Duration, timeout_duration: Duration, connection_timestamp: Timestamp) -> Self {
 
         return ServerConnection {
-            address,
-            connection_timestamp,
-            heartbeat_manager: Timer::new(heartbeat_interval),
-            timeout_manager: Timer::new(timeout_duration),
-            ack_manager: AckManager::new(HostType::Client),
-            event_manager: EventManager::new(),
+            connection: Connection::new(
+                address,
+                connection_timestamp,
+                Timer::new(heartbeat_interval),
+                Timer::new(timeout_duration),
+                AckManager::new(HostType::Client),
+                EventManager::new()
+            ),
             entity_manager: ClientEntityManager::new(),
         };
     }
 
-    pub fn mark_sent(&mut self) {
-        self.heartbeat_manager.reset();
-    }
-
-    pub fn should_send_heartbeat(&self) -> bool {
-        self.heartbeat_manager.ringing()
-    }
-
-    pub fn mark_heard(&mut self) {
-        self.timeout_manager.reset();
-    }
-
-    pub fn should_drop(&self) -> bool {
-        self.timeout_manager.ringing()
-    }
-
-    pub fn process_incoming_header(&mut self, payload: &[u8]) -> Box<[u8]> {
-        self.ack_manager.process_incoming(&mut self.event_manager, &mut Option::<&mut EntityManager<U>>::None, payload)
-    }
-
-    pub fn process_outgoing_header(&mut self, packet_type: PacketType, payload: &[u8]) -> Box<[u8]> {
-        self.ack_manager.process_outgoing(packet_type, payload)
-    }
-
-    pub fn get_next_packet_index(&self) -> SequenceNumber {
-        self.ack_manager.local_sequence_num()
-    }
-
-    pub fn queue_event(&mut self, event: &impl NetEvent<T>) {
-        self.event_manager.queue_outgoing_event(event);
-    }
-
     pub fn get_outgoing_packet(&mut self, event_manifest: &EventManifest<T>) -> Option<Box<[u8]>> {
 
-        if self.event_manager.has_outgoing_events() {
+        if self.connection.has_outgoing_events() {
             let mut writer = PacketWriter::new();
 
             let next_packet_index: u16 = self.get_next_packet_index();
-            while let Some(popped_event) = self.event_manager.pop_outgoing_event(next_packet_index) {
+            while let Some(popped_event) = self.connection.pop_outgoing_event(next_packet_index) {
                 if !writer.write_event(event_manifest, &popped_event) {
-                    self.event_manager.unpop_outgoing_event(next_packet_index, &popped_event);
+                    self.connection.unpop_outgoing_event(next_packet_index, &popped_event);
                     break;
                 }
             }
@@ -90,10 +55,6 @@ impl<T: EventType, U: EntityType> ServerConnection<T, U> {
         return None;
     }
 
-    pub fn get_incoming_event(&mut self) -> Option<T> {
-        return self.event_manager.pop_incoming_event();
-    }
-
     pub fn get_incoming_entity_message(&mut self) -> Option<ClientEntityMessage<U>> {
         return self.entity_manager.pop_incoming_message();
     }
@@ -103,7 +64,7 @@ impl<T: EventType, U: EntityType> ServerConnection<T, U> {
         while reader.has_more() {
             match reader.read_manager_type() {
                 ManagerType::Event => {
-                    self.event_manager.process_data(&mut reader, event_manifest);
+                    self.connection.process_event_data(&mut reader, event_manifest);
                 }
                 ManagerType::Entity => {
                     self.entity_manager.process_data(&mut reader, entity_manifest);
@@ -115,5 +76,43 @@ impl<T: EventType, U: EntityType> ServerConnection<T, U> {
 
     pub fn get_local_entity(&self, key: LocalEntityKey) -> Option<&U> {
         return self.entity_manager.get_local_entity(key);
+    }
+
+    // Pass-through methods to underlying common connection
+
+    pub fn mark_sent(&mut self) {
+        return self.connection.mark_sent();
+    }
+
+    pub fn should_send_heartbeat(&self) -> bool {
+        return self.connection.should_send_heartbeat();
+    }
+
+    pub fn mark_heard(&mut self) {
+        return self.connection.mark_heard();
+    }
+
+    pub fn should_drop(&self) -> bool {
+        return self.connection.should_drop();
+    }
+
+    pub fn process_incoming_header(&mut self, payload: &[u8]) -> Box<[u8]> {
+        return self.connection.process_incoming_header(&mut Option::<&mut ServerEntityManager<U>>::None, payload);
+    }
+
+    pub fn process_outgoing_header(&mut self, packet_type: PacketType, payload: &[u8]) -> Box<[u8]> {
+        return self.connection.process_outgoing_header(packet_type, payload);
+    }
+
+    pub fn get_next_packet_index(&self) -> SequenceNumber {
+        return self.connection.get_next_packet_index();
+    }
+
+    pub fn queue_event(&mut self, event: &impl NetEvent<T>) {
+        return self.connection.queue_event(event);
+    }
+
+    pub fn get_incoming_event(&mut self) -> Option<T> {
+        return self.connection.get_incoming_event();
     }
 }
