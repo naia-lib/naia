1c295168d22a7e4fafcb8dac3f454ce129c8b8f6
Connor Carpenter <connorcarpenter@gmail.com>
2020-06-12 17:50:25 -0600

    moving server & client specific messages & managers out and

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -1,12 +1,8 @@
-pub(crate) mod server_entity_manager;
-pub(crate) mod client_entity_manager;
 pub(crate) mod entity_manifest;
 pub(crate) mod net_entity;
 pub(crate) mod entity_type;
 pub(crate) mod entity_store;
-pub(crate) mod entity_manager;
 pub(crate) mod state_mask;
 pub(crate) mod entity_record;
-pub(crate) mod server_entity_message;
-pub(crate) mod client_entity_message;
 pub(crate) mod mut_handler;
+pub(crate) mod entity_notifiable;
--- src/packet_writer.rs
+++ src/packet_writer.rs
@@ -1,13 +1,13 @@
 use byteorder::{BigEndian, WriteBytesExt};
-use crate::{ManagerType, StandardHeader, NetEvent, NetEventType, EventManifest, EventType, EntityType, EntityManifest, ServerEntityMessage};
+use crate::{ManagerType, StandardHeader, NetEvent, NetEventType, EventManifest, EventType};
 
-const MTU_SIZE: usize = 508 - StandardHeader::bytes_number();
+pub const MTU_SIZE: usize = 508 - StandardHeader::bytes_number();
 
 pub struct PacketWriter {
     event_working_bytes: Vec<u8>,
     event_count: u8,
-    entity_working_bytes: Vec<u8>,
-    entity_message_count: u8,
+    pub entity_working_bytes: Vec<u8>,
+    pub entity_message_count: u8,
 }
 
 impl PacketWriter {
@@ -50,7 +50,7 @@ impl PacketWriter {
         out_bytes.into_boxed_slice()
     }
 
-    fn bytes_number(&self) -> usize {
+    pub fn bytes_number(&self) -> usize {
         return self.event_working_bytes.len() + self.entity_working_bytes.len();
     }
 
@@ -78,66 +78,6 @@ impl PacketWriter {
         if hypothetical_next_payload_size < MTU_SIZE {
             self.event_count += 1;
             self.event_working_bytes.append(&mut event_total_bytes);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    pub fn write_entity_message<T: EntityType>(&mut self, manifest: &EntityManifest<T>, message: &ServerEntityMessage<T>) -> bool {
-
-        let mut entity_total_bytes = Vec::<u8>::new();
-
-        match message {
-            ServerEntityMessage::Create(_, local_key, entity) => {
-
-                //write entity payload
-                let mut entity_payload_bytes = Vec::<u8>::new();
-                entity.as_ref().borrow().write(&mut entity_payload_bytes);
-                if entity_payload_bytes.len() > 255 {
-                    error!("cannot encode an entity with more than 255 bytes, need to implement this");
-                }
-
-                //Write entity "header" (entity id & payload length)
-                entity_total_bytes.write_u8(message.write_message_type()).unwrap(); // write entity message type
-
-                let type_id = entity.as_ref().borrow().get_type_id();
-                let gaia_id = manifest.get_gaia_id(&type_id); // get gaia id
-                entity_total_bytes.write_u16::<BigEndian>(gaia_id).unwrap();// write gaia id
-                entity_total_bytes.write_u16::<BigEndian>(*local_key).unwrap();//write local key
-                entity_total_bytes.write_u8(entity_payload_bytes.len() as u8).unwrap(); // write payload length
-                entity_total_bytes.append(&mut entity_payload_bytes); // write payload
-            }
-            ServerEntityMessage::Delete(_, local_key) => {
-
-                entity_total_bytes.write_u8(message.write_message_type()).unwrap(); //Write entity message type
-                entity_total_bytes.write_u16::<BigEndian>(*local_key).unwrap();//write local key
-            }
-            ServerEntityMessage::Update(_, local_key, state_mask, entity) => {
-                //write entity payload
-                let mut entity_payload_bytes = Vec::<u8>::new();
-                entity.as_ref().borrow().write_partial(state_mask, &mut entity_payload_bytes);
-                if entity_payload_bytes.len() > 255 {
-                    error!("cannot encode an entity with more than 255 bytes, need to implement this");
-                }
-
-                //Write entity "header" (entity id & payload length)
-                entity_total_bytes.write_u8(message.write_message_type()).unwrap(); // write entity message type
-
-                entity_total_bytes.write_u16::<BigEndian>(*local_key).unwrap();//write local key
-                state_mask.as_ref().borrow_mut().write(&mut entity_total_bytes);// write state mask
-                entity_total_bytes.write_u8(entity_payload_bytes.len() as u8).unwrap(); // write payload length
-                entity_total_bytes.append(&mut entity_payload_bytes); // write payload
-            }
-        }
-
-        let mut hypothetical_next_payload_size = self.bytes_number() + entity_total_bytes.len();
-        if self.entity_message_count == 0 {
-            hypothetical_next_payload_size += 2;
-        }
-        if hypothetical_next_payload_size < MTU_SIZE {
-            self.entity_message_count += 1;
-            self.entity_working_bytes.append(&mut entity_total_bytes);
             return true;
         } else {
             return false;
