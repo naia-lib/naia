60d21982d002c493429e90b7b537a3be647bc870
Connor Carpenter <connorcarpenter@gmail.com>
2020-06-12 17:51:22 -0600

    pulling entity_manager & entity_message out of gaia_shared

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,14 @@ pub use gaia_shared::{find_my_ip_address, Config, EntityType};
 mod gaia_client;
 mod server_connection;
 mod client_event;
+mod client_entity_message;
+mod client_entity_manager;
 mod error;
 
 pub use {
     gaia_client::GaiaClient,
     client_event::ClientEvent,
-    gaia_client_socket::Packet
+    gaia_client_socket::Packet,
+    client_entity_message::ClientEntityMessage,
+    client_entity_manager::ClientEntityManager,
 };
