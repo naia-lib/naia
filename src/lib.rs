875a7ce61ff663a181b77798c2f36e1e54850ae6
Connor Carpenter <connorcarpenter@gmail.com>
2020-06-13 05:51:29 -0600

    singular manifest for entities & events

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,13 @@
 #[macro_use]
 extern crate log;
 
-mod event_manifest_load;
-mod entity_manifest_load;
+mod manifest_load;
 mod point_entity;
 mod string_event;
 mod example_event;
 mod example_entity;
 
-pub use event_manifest_load::event_manifest_load;
-pub use entity_manifest_load::entity_manifest_load;
+pub use manifest_load::manifest_load;
 pub use string_event::StringEvent;
 pub use point_entity::PointEntity;
 pub use example_event::ExampleEvent;
