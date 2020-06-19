fb1af28ee0e5d0623cc4b86e4d0fbd5c077f7e18
Connor Carpenter <connorcarpenter@gmail.com>
2020-06-18 18:10:42 -0600

    solving naive client/server handshake

--- src/user.rs
+++ src/user.rs
@@ -2,17 +2,20 @@
 use std::{
     net::SocketAddr,
 };
+use crate::gaia_server::Timestamp;
 
 new_key_type! { pub struct UserKey; }
 
 pub struct User {
     pub address: SocketAddr,
+    pub timestamp: Timestamp,
 }
 
 impl User {
-    pub fn new(address: SocketAddr) -> User {
+    pub fn new(address: SocketAddr, timestamp: Timestamp) -> User {
         User {
-            address
+            address,
+            timestamp,
         }
     }
 }
