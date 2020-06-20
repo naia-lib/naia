dfe1c3946d79318e64ebe91fc3889bca006137f9
Connor Carpenter <connorcarpenter@gmail.com>
2020-06-19 18:21:27 -0600

    implement auth event

--- src/manifest_load.rs
+++ src/manifest_load.rs
@@ -1,12 +1,12 @@
 
 use gaia_shared::{Manifest};
-use crate::{ExampleEvent, StringEvent, ExampleEntity, PointEntity};
+use crate::{ExampleEvent, StringEvent, AuthEvent, ExampleEntity, PointEntity};
 
 pub fn manifest_load() -> Manifest<ExampleEvent, ExampleEntity> {
     let mut manifest = Manifest::<ExampleEvent, ExampleEntity>::new();
 
+    manifest.register_event(&AuthEvent::init());
     manifest.register_event(&StringEvent::init());
-
     manifest.register_entity(&PointEntity::init());
 
     manifest
