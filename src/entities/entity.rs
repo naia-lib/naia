51d6bd4cf23f43c71c0a0c36fad75df70c0a1cf8
naia-rs <naia.rust.crate@gmail.com>
2020-06-22 17:47:05 -0600

    cleanup

--- src/entities/entity.rs
+++ src/entities/entity.rs
@@ -13,6 +13,5 @@ pub trait Entity<T: EntityType> {
     fn write(&self, out_bytes: &mut Vec<u8>);
     fn write_partial(&self, state_mask: &Rc<RefCell<StateMask>>, out_bytes: &mut Vec<u8>);
     fn read_partial(&mut self, state_mask: &StateMask, in_bytes: &[u8]);
-    fn print(&self, key: u16);
     fn set_mutator(&mut self, mutator: &Rc<RefCell<dyn EntityMutator>>);
 }
--- src/entities/entity_type.rs
+++ src/entities/entity_type.rs
@@ -2,6 +2,5 @@ use crate::{StateMask};
 
 pub trait EntityType {
     fn read_partial(&mut self, state_mask: &StateMask, bytes: &[u8]);
-    fn print(&self, key: u16);
     fn clone_inner_rc(&self) -> Self;
 }
