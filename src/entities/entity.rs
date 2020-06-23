c43b6e07b577a8e2f0d7e23fcebe1f9105492a8a
Connor Carpenter <connorcarpenter@gmail.com>
2020-06-22 19:13:49 -0600

    cleanup

--- src/entities/entity.rs
+++ src/entities/entity.rs
@@ -11,7 +11,7 @@ pub trait Entity<T: EntityType> {
     fn to_type(&self) -> T;
     fn get_type_id(&self) -> TypeId;
     fn write(&self, out_bytes: &mut Vec<u8>);
-    fn write_partial(&self, state_mask: &Rc<RefCell<StateMask>>, out_bytes: &mut Vec<u8>);
+    fn write_partial(&self, state_mask: &StateMask, out_bytes: &mut Vec<u8>);
     fn read_partial(&mut self, state_mask: &StateMask, in_bytes: &[u8]);
     fn set_mutator(&mut self, mutator: &Rc<RefCell<dyn EntityMutator>>);
 }
