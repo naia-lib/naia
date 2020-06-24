8a4265fcd06f4bf79c1d5bef962a08c31c9a31f8
naia-rs <naia.rust.crate@gmail.com>
2020-06-23 21:09:06 -0600

    use macro for ExampleEntity

--- src/example_entity.rs
+++ src/example_entity.rs
@@ -4,32 +4,13 @@ use std::{
     cell::RefCell,
 };
 
+use gaia_derive::EntityType;
+
 use gaia_shared::{EntityType, Entity, StateMask};
 
 use crate::{PointEntity};
 
-//TODO: Candidate for Macro (just list names of entity structs ("PointEntity")
+#[derive(EntityType)]
 pub enum ExampleEntity {
     PointEntity(Rc<RefCell<PointEntity>>),
-}
-
-impl EntityType for ExampleEntity {
-
-    //TODO: Candidate for Macro
-    fn read_partial(&mut self, state_mask: &StateMask, bytes: &[u8]) {
-        match self {
-            ExampleEntity::PointEntity(identity) => {
-                identity.as_ref().borrow_mut().read_partial(state_mask, bytes);
-            }
-        }
-    }
-
-    //TODO: Candidate for Macro
-//    fn clone_inner_rc(&self) -> Self {
-//        match self {
-//            ExampleEntity::PointEntity(identity) => {
-//                return ExampleEntity::PointEntity(identity.clone());
-//            }
-//        }
-//    }
 }
