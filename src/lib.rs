4fb5f635a5d33bd4849dc45900e82ad0d35547b3
naia-rs <naia.rust.crate@gmail.com>
2020-06-23 20:49:34 -0600

    it's a start

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,34 @@
 extern crate proc_macro;
-use proc_macro::TokenStream;
 
-#[proc_macro_derive(EventType)]
-pub fn derive_event_type(_item: TokenStream) -> TokenStream {
-    "fn answer() -> u32 { 42 }".parse().unwrap()
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(EntityType)]
+pub fn entity_type_derive(input: TokenStream) -> TokenStream {
+
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation
+    entity_type_impl(&ast)
+}
+
+fn entity_type_impl(ast: &syn::DeriveInput) -> TokenStream {
+
+    let type_name = &ast.ident;
+
+    let gen = quote! {
+        impl EntityType for #type_name {
+            fn read_partial(&mut self, state_mask: &StateMask, bytes: &[u8]) {
+                match self {
+                    #type_name::PointEntity(identity) => {
+                        identity.as_ref().borrow_mut().read_partial(state_mask, bytes);
+                    }
+                }
+            }
+        }
+    };
+    gen.into()
 }
