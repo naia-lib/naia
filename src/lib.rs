83c4064ef7a795b2640ba14896e2129df49fd400
Connor Carpenter <connorcarpenter@gmail.com>
2020-06-23 21:07:21 -0600

    getting proc_macro2 in here..

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,53 @@
-extern crate proc_macro;
-
-use proc_macro::TokenStream;
-use quote::quote;
-use syn;
+use proc_macro2::TokenStream;
+use quote::{quote};
+use syn::{parse_macro_input, Data, DeriveInput, Ident};
 
 #[proc_macro_derive(EntityType)]
-pub fn entity_type_derive(input: TokenStream) -> TokenStream {
+pub fn entity_type_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
-    let ast = syn::parse(input).unwrap();
+    let input = parse_macro_input!(input as DeriveInput);
 
-    // Build the trait implementation
-    entity_type_impl(&ast)
-}
+    let type_name = input.ident;
 
-fn entity_type_impl(ast: &syn::DeriveInput) -> TokenStream {
-
-    let type_name = &ast.ident;
+    let variants = get_variants(&type_name, &input.data);
 
     let gen = quote! {
         impl EntityType for #type_name {
             fn read_partial(&mut self, state_mask: &StateMask, bytes: &[u8]) {
                 match self {
-                    #type_name::PointEntity(identity) => {
-                        identity.as_ref().borrow_mut().read_partial(state_mask, bytes);
-                    }
+                    #variants
                 }
             }
         }
     };
-    gen.into()
+
+    proc_macro::TokenStream::from(gen)
 }
+
+fn get_variants(type_name: &Ident, data: &Data) -> TokenStream {
+    quote ! {
+        #type_name::PointEntity(identity) => {
+            identity.as_ref().borrow_mut().read_partial(state_mask, bytes);
+        }
+    }
+}
+
+
+////FROM THIS
+//#[derive(EntityType)]
+//pub enum ExampleEntity {
+//    PointEntity(Rc<RefCell<PointEntity>>),
+//}
+
+////TO THIS
+//impl EntityType for ExampleEntity {
+//
+//    //TODO: Candidate for Macro
+//    fn read_partial(&mut self, state_mask: &StateMask, bytes: &[u8]) {
+//        match self {
+//            ExampleEntity::PointEntity(identity) => {
+//                identity.as_ref().borrow_mut().read_partial(state_mask, bytes);
+//            }
+//        }
+//    }
+//}
