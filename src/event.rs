12598a7c2963c488f7403e10114f3bbd6ea45011
naia-rs <naia.rust.crate@gmail.com>
2020-06-25 15:09:12 -0600

    event write method now deriving correctly

--- src/event.rs
+++ src/event.rs
@@ -1,15 +1,17 @@
 use proc_macro2::{TokenStream, Span};
 use quote::{quote};
-use syn::{parse_macro_input, Data, DeriveInput, Ident, Meta, Lit, MetaNameValue};
+use syn::{parse_macro_input, Data, DeriveInput, Ident, Meta, Lit, MetaNameValue, Fields, Field};
 
 pub fn event_impl(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let input = parse_macro_input!(input as DeriveInput);
 
-    let event_name = input.ident;
+    let event_name = &input.ident;
     let event_builder_name = Ident::new((event_name.to_string() + "Builder").as_str(), Span::call_site());
 
     let mut type_name: Option<Ident> = None;
+
+    let properties = get_properties(&input);
 
     for option in input.attrs.into_iter() {
         let option = option.parse_meta().unwrap();
@@ -34,6 +36,8 @@ pub fn event_impl(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         panic!("#[derive(Event)] requires an accompanying #[type_name = \"{Event Type Name Here}\"] attribute");
     }
 
+    let event_write_method = get_event_write_method(&properties);
+
     let gen = quote! {
         pub struct #event_builder_name {
             type_id: TypeId,
@@ -57,9 +61,7 @@ pub fn event_impl(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             fn is_guaranteed(&self) -> bool {
                 #event_name::is_guaranteed()
             }
-            fn write(&self, buffer: &mut Vec<u8>) {
-                PropertyIo::write(&self.message, buffer);
-            }
+            #event_write_method
             fn get_typed_copy(&self) -> ExampleEvent {
                 return ExampleEvent::StringEvent(self.clone());
             }
@@ -69,9 +71,51 @@ pub fn event_impl(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
     };
 
-    //TODO: write() method directly above (in impl Event<#type_name> fpr #event_name block) needs to be implemented!
-
     proc_macro::TokenStream::from(gen)
+}
+
+fn get_properties(input: &DeriveInput) -> Vec<Field> {
+    let mut fields = Vec::new();
+
+    if let Data::Struct(data_struct) = &input.data {
+        if let Fields::Named(fields_named) = &data_struct.fields {
+            for field in fields_named.named.iter() {
+                fields.push(field.clone());
+            }
+        }
+    }
+
+    fields
+}
+
+fn get_event_write_method(properties: &Vec<Field>) -> TokenStream {
+
+    /* quote! {
+        fn write(&self, buffer: &mut Vec<u8>) {
+            PropertyIo::write(&self.message, buffer);
+        }
+    }; */
+
+    let mut output = quote! {};
+
+    for field in properties.iter() {
+        if let Some(property_name) = &field.ident {
+            let new_output_right = quote! {
+                PropertyIo::write(&self.#property_name, buffer);
+            };
+            let new_output_result = quote! {
+                #output
+                #new_output_right
+            };
+            output = new_output_result;
+        }
+    }
+
+    return quote! {
+        fn write(&self, buffer: &mut Vec<u8>) {
+            #output
+        }
+    }
 }
 
 ////FROM THIS
