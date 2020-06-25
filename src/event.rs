b8e550e2dbe4d281fefe4c4afa7c85e95e0ab2e0
naia-rs <naia.rust.crate@gmail.com>
2020-06-25 12:33:13 -0600

    test commit 2

--- src/event.rs
+++ src/event.rs
@@ -119,7 +119,6 @@ pub fn event_impl(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 //        });
 //    }
 //}
-//
 //impl Event<ExampleEvent> for StringEvent {
 //    fn is_guaranteed(&self) -> bool {
 //        StringEvent::is_guaranteed()
