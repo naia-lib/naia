cb66569f39a296abc7aa2204db3a63c38c279563
naia-rs <naia.rust.crate@gmail.com>
2020-07-07 15:24:11 -0600

    - Manually send all Server updates with Server.send_all_updates(). This allows finer control over when to send Server updates rather than just with every tick. - Rename Server.send_event() to Server.queue_event(), to indicate the previous change

--- examples/server/src/main.rs
+++ examples/server/src/main.rs
@@ -108,13 +108,18 @@ async fn main() {
                             info!("Naia Server send -> {}: {}", user.address, new_message);
 
                             let string_event = StringEvent::new(new_message);
-                            server.send_event(&user_key, &string_event);
+                            server.queue_event(&user_key, &string_event);
                         }
 
                         // Iterate through Point Entities, marching them from (0,0) to (20, N)
                         for point_entity in &point_entities {
                             point_entity.borrow_mut().step();
                         }
+
+                        // VERY IMPORTANT! Calling this actually sends all Entity/Event data packets
+                        // to all Clients that require it. If you don't call this method, the Server
+                        // will never communicate with it's connected Clients
+                        server.send_all_updates().await;
 
                         tick_count += 1;
                     }
