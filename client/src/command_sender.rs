785d991d17b458279976dc7051d1b5dcb5e11dbc
Connor Carpenter <naia.rust.crate@gmail.com>
2020-08-30 13:00:56 -0600

    Commands are buffered on the Server and only popped up to the App on the appropriate Tick

--- client/src/command_sender.rs
+++ client/src/command_sender.rs
@@ -5,37 +5,37 @@ use naia_shared::{Event, EventClone, EventType};
 /// Handles outgoing Commands
 #[derive(Debug)]
 pub struct CommandSender<T: EventType> {
-    queued_outgoing_events: VecDeque<Rc<Box<dyn Event<T>>>>,
+    queued_outgoing_commands: VecDeque<Rc<Box<dyn Event<T>>>>,
 }
 
 impl<T: EventType> CommandSender<T> {
     /// Creates a new CommandSender
     pub fn new() -> Self {
         CommandSender {
-            queued_outgoing_events: VecDeque::new(),
+            queued_outgoing_commands: VecDeque::new(),
         }
     }
 
     /// Gets the next queued Command to be transmitted
     pub fn has_command(&self) -> bool {
-        self.queued_outgoing_events.len() != 0
+        self.queued_outgoing_commands.len() != 0
     }
 
     /// Gets the next queued Command to be transmitted
     pub fn pop_command(&mut self) -> Option<Rc<Box<dyn Event<T>>>> {
-        self.queued_outgoing_events.pop_front()
+        self.queued_outgoing_commands.pop_front()
     }
 
     /// If  the last popped Command from the queue somehow wasn't able to be
     /// written into a packet, put the Command back into the front of the queue
-    pub fn unpop_command(&mut self, event: &Rc<Box<dyn Event<T>>>) {
-        let cloned_event = event.clone();
-        self.queued_outgoing_events.push_front(cloned_event);
+    pub fn unpop_command(&mut self, command: &Rc<Box<dyn Event<T>>>) {
+        let cloned_command = command.clone();
+        self.queued_outgoing_commands.push_front(cloned_command);
     }
 
     /// Queues an Command to be transmitted to the remote host
-    pub fn queue_command(&mut self, event: &impl Event<T>) {
-        let clone = Rc::new(EventClone::clone_box(event));
-        self.queued_outgoing_events.push_back(clone);
+    pub fn queue_command(&mut self, command: &impl Event<T>) {
+        let clone = Rc::new(EventClone::clone_box(command));
+        self.queued_outgoing_commands.push_back(clone);
     }
 }
