447c63736e79a4e660e88b56fd3e0c823e14f176
Connor Carpenter <naia.rust.crate@gmail.com>
2020-09-05 22:01:48 -0600

    fixed some of the errors with writing pawn state & storing commands on the correct frame.. Clientside prediction seems to be working for now!

--- shared/src/entities/entity_type.rs
+++ shared/src/entities/entity_type.rs
@@ -5,6 +5,8 @@ use super::{entity::Entity, state_mask::StateMask};
 /// An Enum with a variant for every Entity that can be synced between
 /// Client/Host
 pub trait EntityType<Impl = Self>: Clone {
+    /// Read bytes from an incoming packet into all contained Properties
+    fn read_full(&mut self, bytes: &[u8], packet_index: u16);
     /// Read bytes from an incoming packet, updating the Properties which have
     /// been mutated on the Server
     fn read_partial(&mut self, state_mask: &StateMask, bytes: &[u8], packet_index: u16);
