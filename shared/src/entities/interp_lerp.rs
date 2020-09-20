d320dc6b42a55507e9e006717d29398618e7c6ed
Connor Carpenter <naia.rust.crate@gmaiol.com>
2020-09-19 21:25:01 -0600

    debugging

--- shared/src/entities/interp_lerp.rs
+++ shared/src/entities/interp_lerp.rs
@@ -1,16 +1,28 @@
 use std::ops::{Add, Sub};
 
-pub trait InterpLerpable: Sized + Sub + Add {
+pub trait InterpLerpable: Sized + Sub + Add + Clone + Copy + PartialEq {
     fn to_f32(&self) -> f32;
     fn from_f32(input: f32) -> Self;
 }
 
 /// Returns an interpolation from one value to another by a specified amount
 pub fn interp_lerp<T: InterpLerpable>(old_value: &T, new_value: &T, fraction: f32) -> T {
+    if fraction == 0.0 || PartialEq::eq(old_value, new_value) {
+        return (*old_value).clone();
+    }
+    if fraction == 1.0 {
+        return (*new_value).clone();
+    }
     let old_float: f32 = old_value.to_f32();
     let new_float: f32 = new_value.to_f32();
     let output_f32 = ((new_float - old_float) * fraction) + old_float;
     let output: T = T::from_f32(output_f32);
+    if fraction != 1.0 {
+        println!(
+            "interplerp: old: {}, new: {}, frac: {}, result: {}",
+            old_float, new_float, fraction, output_f32
+        );
+    }
     output
 }
 
