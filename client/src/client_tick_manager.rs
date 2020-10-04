bcb572fdae5c6bf184e9fbafe249ceb310d57f3d
Connor Carpenter <naia.rust.crate@gmaiol.com>
2020-10-04 13:33:12 -0600

    frames now generate ticks, which triggers an update of all entities. massively simplifying how many timers are ticking at all times

--- client/src/client_tick_manager.rs
+++ client/src/client_tick_manager.rs
@@ -1,104 +1,67 @@
 use std::time::Duration;
 
-use naia_shared::{wrapping_diff, HostTickManager, Instant};
+use naia_shared::{wrapping_diff, Instant};
 
 /// Manages the current tick for the host
 #[derive(Debug)]
 pub struct ClientTickManager {
     tick_interval: Duration,
-    client_tick: u16,
+    tick_interval_f32: f32,
     server_tick: u16,
     client_tick_adjust: u16,
     server_tick_adjust: u16,
-    last_client_tick_instant: Instant,
-    last_client_tick_leftover: Duration,
-    last_server_tick_instant: Instant,
-    last_server_tick_leftover: Duration,
     server_tick_running_diff: i16,
+    last_tick_instant: Instant,
+    pub fraction: f32,
+    accumulator: f32,
+    has_ticked: bool,
 }
-
-const NANOS_PER_SEC: u32 = 1_000_000_000;
 
 impl ClientTickManager {
     /// Create a new HostTickManager with a given tick interval duration
     pub fn new(tick_interval: Duration) -> Self {
         ClientTickManager {
             tick_interval,
-            client_tick: 0,
+            tick_interval_f32: tick_interval.as_nanos() as f32 / 1000000000.0,
             server_tick: 1,
-            last_client_tick_instant: Instant::now(),
-            last_client_tick_leftover: Duration::new(0, 0),
-            last_server_tick_instant: Instant::now(),
-            last_server_tick_leftover: Duration::new(0, 0),
             client_tick_adjust: 0,
             server_tick_adjust: 0,
             server_tick_running_diff: 0,
+            last_tick_instant: Instant::now(),
+            accumulator: 0.0,
+            fraction: 0.0,
+            has_ticked: false,
         }
+    }
+
+    pub fn mark_frame(&mut self) -> bool {
+        let mut ticked = false;
+        let mut frame_time = self.last_tick_instant.elapsed().as_nanos() as f32 / 1000000000.0;
+        if frame_time > 0.25 {
+            frame_time = 0.25;
+        }
+        self.accumulator += frame_time;
+        self.last_tick_instant = Instant::now();
+        if self.accumulator >= self.tick_interval_f32 {
+            while self.accumulator >= self.tick_interval_f32 {
+                self.accumulator -= self.tick_interval_f32;
+            }
+            // tick has occurred
+            ticked = true;
+            self.has_ticked = true;
+            self.server_tick = self.server_tick.wrapping_add(1);
+        }
+        self.fraction = self.accumulator / self.tick_interval_f32;
+        ticked
     }
 
     /// If the tick interval duration has elapsed, increment the current tick
-    pub fn has_ticked(&mut self) -> bool {
-        // Server Tick
-        {
-            let mut time_elapsed =
-                self.last_server_tick_instant.elapsed() + self.last_server_tick_leftover;
-
-            if time_elapsed > self.tick_interval {
-                while time_elapsed > self.tick_interval {
-                    self.server_tick = self.server_tick.wrapping_add(1);
-                    time_elapsed -= self.tick_interval;
-                }
-
-                self.last_server_tick_leftover = time_elapsed;
-                self.last_server_tick_instant = Instant::now();
-            }
+    pub fn take_tick(&mut self) -> bool {
+        if self.has_ticked {
+            self.has_ticked = false;
+            return true;
         }
-
-        // Client Tick
-        {
-            let mut time_elapsed =
-                self.last_client_tick_instant.elapsed() + self.last_client_tick_leftover;
-
-            let intended_diff = wrapping_diff(
-                self.client_tick,
-                self.server_tick.wrapping_add(self.client_tick_adjust),
-            )
-            .min(20)
-            .max(-20);
-            let tick_factor = 2.0_f64.powf(-0.2_f64 * f64::from(intended_diff));
-            let adjusted_interval = self.get_adjusted_duration(tick_factor);
-
-            if time_elapsed > adjusted_interval {
-                self.client_tick = self.client_tick.wrapping_add(1);
-                time_elapsed -= adjusted_interval;
-
-                self.last_client_tick_leftover = time_elapsed;
-                self.last_client_tick_instant = Instant::now();
-
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-    fn get_adjusted_duration(&self, tick_factor: f64) -> Duration {
-        const MAX_NANOS_F64: f64 = ((std::u64::MAX as u128 + 1) * (NANOS_PER_SEC as u128)) as f64;
-        let nanos = tick_factor * self.tick_interval.as_secs_f64() * (NANOS_PER_SEC as f64);
-        if !nanos.is_finite() {
-            panic!("got non-finite value when converting float to duration");
-        }
-        if nanos >= MAX_NANOS_F64 {
-            panic!("overflow when converting float to duration");
-        }
-        if nanos < 0.0 {
-            panic!("underflow when converting float to duration");
-        }
-        let nanos_u128 = nanos as u128;
-        Duration::new(
-            (nanos_u128 / (NANOS_PER_SEC as u128)) as u64,
-            (nanos_u128 % (NANOS_PER_SEC as u128)) as u32,
-        )
+        return false;
     }
 
     /// Use tick data from initial server handshake to set the initial tick
@@ -107,10 +70,6 @@ impl ClientTickManager {
         self.server_tick_adjust = ((1000 / (self.tick_interval.as_millis())) + 1) as u16;
 
         self.client_tick_adjust = ((3000 / (self.tick_interval.as_millis())) + 1) as u16;
-        self.client_tick = server_tick.wrapping_add(self.client_tick_adjust);
-
-        self.last_server_tick_instant = Instant::now();
-        self.last_client_tick_instant = Instant::now();
     }
 
     /// Using information from the Server and RTT/Jitter measurements, determine
@@ -128,7 +87,7 @@ impl ClientTickManager {
                     "Adding! Client: {}, Server: {}",
                     self.server_tick, server_tick,
                 );
-                self.server_tick = self.server_tick.wrapping_add(1);
+                self.server_tick = server_tick;
                 self.server_tick_running_diff = 0;
             }
 
@@ -140,7 +99,7 @@ impl ClientTickManager {
                     "Subing! Client: {}, Server: {}",
                     self.server_tick, server_tick,
                 );
-                self.server_tick = self.server_tick.wrapping_sub(1);
+                self.server_tick = server_tick;
                 self.server_tick_running_diff = 0;
             }
 
@@ -161,14 +120,13 @@ impl ClientTickManager {
         return &self.tick_interval;
     }
 
-    /// Gets the server tick with the jitter buffer offset applied
-    pub fn get_buffered_server_tick(&self) -> u16 {
+    /// Gets the server tick with the incoming jitter buffer offset applied
+    pub fn get_server_tick(&self) -> u16 {
         return self.server_tick.wrapping_sub(self.server_tick_adjust);
     }
-}
 
-impl HostTickManager for ClientTickManager {
-    fn get_tick(&self) -> u16 {
-        self.client_tick
+    /// Gets the client tick with the outgoing jitter buffer offset applied
+    pub fn get_client_tick(&self) -> u16 {
+        return self.server_tick.wrapping_add(self.client_tick_adjust);
     }
 }
--- client/src/interpolation_manager.rs
+++ client/src/interpolation_manager.rs
@@ -1,64 +1,40 @@
 use std::collections::HashMap;
 
-use crate::client_entity_manager::ClientEntityManager;
-use naia_shared::{EntityType, Instant, LocalEntityKey};
+use crate::{client_entity_manager::ClientEntityManager, client_tick_manager::ClientTickManager};
+use naia_shared::{EntityType, LocalEntityKey};
 use std::time::Duration;
 
 #[derive(Debug)]
 pub struct InterpolationManager<U: EntityType> {
-    ///////////////////////////////////old_tick, recent_tick, temp_entity, old_entity, prev_entity,
-    ///////////////////////////////////old_tick, next_entity, fresh_old
+    ////////temp_entity, prev_entity, next_entity
     entity_store: HashMap<LocalEntityKey, (U, U, U)>,
     pawn_store: HashMap<LocalEntityKey, (U, U, U)>,
     interp_duration: f32,
-    tick_duration: f32,
-    last_tick_instant: Instant,
-    last_tick: u16,
-    fraction: f32,
-    accumulator: f32,
 }
 
 impl<U: EntityType> InterpolationManager<U> {
-    pub fn new(tick_duration: &Duration, server_tick: u16) -> Self {
+    pub fn new(tick_duration: &Duration) -> Self {
         InterpolationManager {
             entity_store: HashMap::new(),
             pawn_store: HashMap::new(),
-            tick_duration: tick_duration.as_nanos() as f32 / 1000000000.0,
             interp_duration: tick_duration.as_millis() as f32,
-            last_tick: server_tick,
-            last_tick_instant: Instant::now(),
-            accumulator: 0.0,
-            fraction: 0.0,
         }
     }
 
-    pub fn mark(&mut self, entity_manager: &ClientEntityManager<U>) {
-        let mut frame_time = self.last_tick_instant.elapsed().as_nanos() as f32 / 1000000000.0;
-        if frame_time > 0.25 {
-            frame_time = 0.25;
+    pub fn tick(&mut self, entity_manager: &ClientEntityManager<U>) {
+        for (key, (_, prev_ent, next_ent)) in self.entity_store.iter_mut() {
+            if let Some(now_ent) = entity_manager.get_entity(key) {
+                prev_ent.mirror(next_ent);
+                next_ent.mirror(now_ent);
+            }
         }
-        self.accumulator += frame_time;
-        self.last_tick_instant = Instant::now();
-        if self.accumulator >= self.tick_duration {
-            while self.accumulator >= self.tick_duration {
-                self.accumulator -= self.tick_duration;
-            }
-            // tick
-            for (key, (_, prev_ent, next_ent)) in self.entity_store.iter_mut() {
-                if let Some(now_ent) = entity_manager.get_entity(key) {
-                    prev_ent.mirror(next_ent);
-                    next_ent.mirror(now_ent);
-                }
-            }
 
-            for (key, (_, prev_ent, next_ent)) in self.pawn_store.iter_mut() {
-                if let Some(now_ent) = entity_manager.get_pawn(key) {
-                    prev_ent.mirror(next_ent);
-                    next_ent.mirror(now_ent);
-                }
+        for (key, (_, prev_ent, next_ent)) in self.pawn_store.iter_mut() {
+            if let Some(now_ent) = entity_manager.get_pawn(key) {
+                prev_ent.mirror(next_ent);
+                next_ent.mirror(now_ent);
             }
         }
-        self.fraction = self.accumulator / self.tick_duration;
     }
 
     // entities
@@ -92,9 +68,13 @@ impl<U: EntityType> InterpolationManager<U> {
         self.entity_store.remove(key);
     }
 
-    pub fn get_interpolation(&mut self, key: &LocalEntityKey) -> Option<&U> {
+    pub fn get_interpolation(
+        &mut self,
+        tick_manager: &ClientTickManager,
+        key: &LocalEntityKey,
+    ) -> Option<&U> {
         if let Some((temp_entity, prev_entity, next_entity)) = self.entity_store.get_mut(key) {
-            temp_entity.set_to_interpolation(prev_entity, next_entity, self.fraction);
+            temp_entity.set_to_interpolation(prev_entity, next_entity, tick_manager.fraction);
             return Some(temp_entity);
         }
         return None;
@@ -131,9 +111,13 @@ impl<U: EntityType> InterpolationManager<U> {
         self.pawn_store.remove(key);
     }
 
-    pub fn get_pawn_interpolation(&mut self, key: &LocalEntityKey) -> Option<&U> {
+    pub fn get_pawn_interpolation(
+        &mut self,
+        tick_manager: &ClientTickManager,
+        key: &LocalEntityKey,
+    ) -> Option<&U> {
         if let Some((temp_entity, prev_entity, next_entity)) = self.pawn_store.get_mut(key) {
-            temp_entity.set_to_interpolation(prev_entity, next_entity, self.fraction);
+            temp_entity.set_to_interpolation(prev_entity, next_entity, tick_manager.fraction);
             return Some(temp_entity);
         }
         return None;
