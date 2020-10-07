6a87b7620837f9cc0c9dbae0635ed32ec87eacca
Connor Carpenter <naia.rust.crate@gmaiol.com>
2020-10-06 20:42:06 -0600

    cleanup, and added method on server to get count of entities/users/rooms

--- shared/src/entities/property_io.rs
+++ shared/src/entities/property_io.rs
@@ -9,7 +9,7 @@ use super::property::Property;
 use crate::wrapping_number::sequence_greater_than;
 
 /// A Property that can read/write itself from/into incoming/outgoing packets
-pub trait PropertyIo<T> {
+pub trait PropertyIo<T: Clone + DeBin + SerBin + PartialEq> {
     /// Writes contained value into outgoing byte stream
     fn write(&self, buffer: &mut Vec<u8>);
     /// Given a cursor into incoming packet data, updates the Property with the
@@ -20,7 +20,7 @@ pub trait PropertyIo<T> {
     fn read_seq(&mut self, cursor: &mut Cursor<&[u8]>, packet_index: u16);
 }
 
-impl<T: Clone + DeBin + SerBin> PropertyIo<T> for Property<T> {
+impl<T: Clone + DeBin + SerBin + PartialEq> PropertyIo<T> for Property<T> {
     fn write(&self, buffer: &mut Vec<u8>) {
         let encoded = &mut SerBin::serialize_bin(&self.inner);
         buffer.push(encoded.len() as u8);
