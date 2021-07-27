7bea41263d88fdd5667005e66d068c1f70ef749c
Connor Carpenter <naia.rust.crate@gmail.com>
2021-07-27 15:07:22 -0600

    server address is now built into client config

--- demo/client/app/src/lib.rs
+++ demo/client/app/src/lib.rs
@@ -2,3 +2,6 @@ extern crate log;
 
 mod app;
 pub use app::App;
+
+mod config;
+pub use config::Config;
--- demo/client/wasm_bindgen/src/lib.rs
+++ demo/client/wasm_bindgen/src/lib.rs
@@ -10,14 +10,14 @@ cfg_if! {
 
         use wasm_bindgen::prelude::*;
 
-        use naia_client_example_app::App;
+        use naia_client_example_app::{App, Config};
 
         #[wasm_bindgen(start)]
         pub fn main_js() {
             // Uncomment the line below to enable logging. You don't need it if something else (e.g. quicksilver) is logging for you
             wasm_logger::init(wasm_logger::Config::default());
 
-            let app = App::new();
+            let app = App::new(Config::get());
 
             loop_wasm::start_loop(app);
         }
