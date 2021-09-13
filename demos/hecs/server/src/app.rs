d369208ab8062999c42e2aff0d95d7e86f912f78
Connor Carpenter <naia.rust.crate@gmail.com>
2021-09-13 15:41:13 -0600

    simplify server init

--- demos/hecs/server/src/app.rs
+++ demos/hecs/server/src/app.rs
@@ -26,10 +26,9 @@ impl App {
     pub fn new() -> Self {
         info!("Naia Hecs Server Demo started");
 
-        let shared_config = get_shared_config();
         let mut server_config = ServerConfig::default();
         server_config.socket_config.session_listen_addr = get_server_address();
-        let mut server = Server::new(Some(server_config), shared_config);
+        let mut server = Server::new(Some(server_config), get_shared_config());
 
         // Create a new, singular room, which will contain Users and Entities that they
         // can receive updates from
