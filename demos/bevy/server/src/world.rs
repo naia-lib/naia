44df6fb5f33d976500190e4a638554668c883de7
Connor Carpenter <connorcarpenter@gmail.com>
2021-09-30 13:03:02 -0600

    bevy server demo is working ... just need to implement WorldType now..

--- demos/bevy/server/src/world.rs
+++ demos/bevy/server/src/world.rs
@@ -10,15 +10,15 @@ use naia_server::{ProtocolType, WorldType, Replicate, ImplRef, Ref, KeyType};
 // Key
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
-pub struct Key(Entity);
+pub struct EntityKey(Entity);
 
-impl Key {
+impl EntityKey {
     pub fn new(entity: Entity) -> Self {
-        return Key(entity);
+        return EntityKey(entity);
     }
 }
 
-impl KeyType for Key {}
+impl KeyType for EntityKey {}
 
 // WorldMetadata
 
@@ -36,62 +36,61 @@ impl WorldMetadata {
 
 // WorldRef
 
-pub struct WorldRef {
-    world: &World,
+pub struct WorldRef<'w> {
+    world: &'w World,
 }
 
-impl WorldRef {
-    pub fn new(world: &World) -> Self {
+impl<'w> WorldRef<'w> {
+    pub fn new(world: &'w World) -> Self {
         WorldRef {
             world,
         }
     }
 }
 
-impl<P: 'static + ProtocolType> WorldType<P> for WorldRef {
-    type EntityKey = Key;
+impl<'w, P: 'static + ProtocolType> WorldType<P, EntityKey> for WorldRef<'w> {
 
-    fn has_entity(&self, entity_key: &Self::EntityKey) -> bool {
+    fn has_entity(&self, entity_key: &EntityKey) -> bool {
         unimplemented!()
     }
 
-    fn entities(&self) -> Vec<Self::EntityKey> {
+    fn entities(&self) -> Vec<EntityKey> {
         unimplemented!()
     }
 
-    fn spawn_entity(&mut self) -> Self::EntityKey {
+    fn spawn_entity(&mut self) -> EntityKey {
         unimplemented!()
     }
 
-    fn despawn_entity(&mut self, entity_key: &Self::EntityKey) {
+    fn despawn_entity(&mut self, entity_key: &EntityKey) {
         unimplemented!()
     }
 
-    fn has_component<R: Replicate<P>>(&self, entity_key: &Self::EntityKey) -> bool {
+    fn has_component<R: Replicate<P>>(&self, entity_key: &EntityKey) -> bool {
         unimplemented!()
     }
 
-    fn has_component_of_type(&self, entity_key: &Self::EntityKey, type_id: &TypeId) -> bool {
+    fn has_component_of_type(&self, entity_key: &EntityKey, type_id: &TypeId) -> bool {
         unimplemented!()
     }
 
-    fn get_component<R: Replicate<P>>(&self, entity_key: &Self::EntityKey) -> Option<Ref<R>> {
+    fn get_component<R: Replicate<P>>(&self, entity_key: &EntityKey) -> Option<Ref<R>> {
         unimplemented!()
     }
 
-    fn get_component_from_type(&self, entity_key: &Self::EntityKey, type_id: &TypeId) -> Option<P> {
+    fn get_component_from_type(&self, entity_key: &EntityKey, type_id: &TypeId) -> Option<P> {
         unimplemented!()
     }
 
-    fn get_components(&self, entity_key: &Self::EntityKey) -> Vec<P> {
+    fn get_components(&self, entity_key: &EntityKey) -> Vec<P> {
         unimplemented!()
     }
 
-    fn insert_component<R: ImplRef<P>>(&mut self, entity_key: &Self::EntityKey, component_ref: R) {
+    fn insert_component<R: ImplRef<P>>(&mut self, entity_key: &EntityKey, component_ref: R) {
         unimplemented!()
     }
 
-    fn remove_component<R: Replicate<P>>(&mut self, entity_key: &Self::EntityKey) {
+    fn remove_component<R: Replicate<P>>(&mut self, entity_key: &EntityKey) {
         unimplemented!()
     }
 }
