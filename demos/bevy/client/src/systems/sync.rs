a03196a9cb27da9936f4db2c96320c2fa83a6b26
Connor Carpenter <naia.rust.crate@gmail.com>
2021-10-12 08:30:01 -0600

    cleanup

--- demos/bevy/client/src/systems/sync.rs
+++ demos/bevy/client/src/systems/sync.rs
@@ -1,30 +1,18 @@
 use bevy::{
-    ecs::{query::With, system::Query},
+    ecs::system::Query,
     transform::components::Transform,
 };
 
-use naia_bevy_client::{
-    components::{Confirmed, Predicted},
-    Ref,
-};
+use naia_bevy_client::Ref;
 
 use naia_bevy_demo_shared::protocol::Position;
 
 pub fn sync(
-    mut predicted: Query<(&Ref<Position>, &mut Transform), With<Predicted>>,
-    mut confirmed: Query<(&Ref<Position>, &mut Transform), With<Confirmed>>,
+    mut query: Query<(&Ref<Position>, &mut Transform)>
 ) {
-    for (pos_ref, mut transform) in confirmed.iter_mut() {
-        sync_transform(pos_ref, &mut transform);
+    for (pos_ref, mut transform) in query.iter_mut() {
+        let pos = pos_ref.borrow();
+        transform.translation.x = f32::from(*(pos.x.get()));
+        transform.translation.y = f32::from(*(pos.y.get())) * -1.0;
     }
-
-    for (pos_ref, mut transform) in predicted.iter_mut() {
-        sync_transform(pos_ref, &mut transform);
-    }
-}
-
-fn sync_transform(pos_ref: &Ref<Position>, transform: &mut Transform) {
-    let pos = pos_ref.borrow();
-    transform.translation.x = f32::from(*(pos.x.get()));
-    transform.translation.y = f32::from(*(pos.y.get())) * -1.0;
 }
