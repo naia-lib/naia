5dec07198806037f7f8612a2b58436b4579cf299
Connor Carpenter <naia.rust.crate@gmail.com>
2021-10-12 16:34:46 -0600

    hecs demo working again

--- adapters/hecs/shared/src/component_access.rs
+++ adapters/hecs/shared/src/component_access.rs
@@ -8,7 +8,8 @@ use super::entity::Entity;
 
 // ComponentAccess
 pub trait ComponentAccess<P: ProtocolType> {
-    fn get_component(&self, world: &World, entity_key: &Entity) -> Option<P>;
+    fn get_component(&self, world: &World, entity: &Entity) -> Option<P>;
+    fn remove_component(&self, world: &mut World, entity: &Entity) -> Option<P>;
 }
 
 // ComponentAccessor
@@ -34,10 +35,23 @@ impl<P: ProtocolType, R: ImplRef<P>> ComponentAccess<P> for ComponentAccessor<P,
         }
         return None;
     }
+
+    fn remove_component(&self, world: &mut World, entity: &Entity) -> Option<P> {
+        if let Some(component_ref) = remove_component_ref::<P, R>(world, entity) {
+            return Some(component_ref.protocol());
+        }
+        return None;
+    }
 }
 
 fn get_component_ref<P: ProtocolType, R: ImplRef<P>>(world: &World, entity: &Entity) -> Option<R> {
     return world
         .get::<R>(**entity)
         .map_or(None, |v| Some(v.deref().clone_ref()));
+}
+
+fn remove_component_ref<P: ProtocolType, R: ImplRef<P>>(world: &mut World, entity: &Entity) -> Option<R> {
+    return world
+        .remove_one::<R>(**entity)
+        .map_or(None, |v| Some(v.clone_ref()));
 }
