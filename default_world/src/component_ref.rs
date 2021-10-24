fb1443467cdf53078f32f8d32b518672c93b5859
Connor Carpenter <naia.rust.crate@gmail.com>
2021-10-23 19:12:17 -0600

    renaming

--- default_world/src/component_ref.rs
+++ default_world/src/component_ref.rs
@@ -2,13 +2,13 @@ use std::marker::PhantomData;
 
 use naia_shared::{ProtocolType, ReplicaMutTrait, ReplicaRefTrait, ReplicateSafe};
 
-// RefWrapper
-pub struct RefWrapper<'a, P: ProtocolType, R: ReplicateSafe<P>> {
+// ComponentRef
+pub struct ComponentRef<'a, P: ProtocolType, R: ReplicateSafe<P>> {
     inner: &'a R,
     phantom: PhantomData<P>,
 }
 
-impl<'a, P: ProtocolType, R: ReplicateSafe<P>> RefWrapper<'a, P, R> {
+impl<'a, P: ProtocolType, R: ReplicateSafe<P>> ComponentRef<'a, P, R> {
     pub fn new(inner: &'a R) -> Self {
         Self {
             inner,
@@ -17,19 +17,19 @@ impl<'a, P: ProtocolType, R: ReplicateSafe<P>> RefWrapper<'a, P, R> {
     }
 }
 
-impl<'a, P: ProtocolType, R: ReplicateSafe<P>> ReplicaRefTrait<P, R> for RefWrapper<'a, P, R> {
+impl<'a, P: ProtocolType, R: ReplicateSafe<P>> ReplicaRefTrait<P, R> for ComponentRef<'a, P, R> {
     fn to_ref(&self) -> &R {
         return &self.inner;
     }
 }
 
-// MutWrapper
-pub struct MutWrapper<'a, P: ProtocolType, R: ReplicateSafe<P>> {
+// ComponentMut
+pub struct ComponentMut<'a, P: ProtocolType, R: ReplicateSafe<P>> {
     inner: &'a mut R,
     phantom: PhantomData<P>,
 }
 
-impl<'a, P: ProtocolType, R: ReplicateSafe<P>> MutWrapper<'a, P, R> {
+impl<'a, P: ProtocolType, R: ReplicateSafe<P>> ComponentMut<'a, P, R> {
     pub fn new(inner: &'a mut R) -> Self {
         Self {
             inner,
@@ -38,13 +38,13 @@ impl<'a, P: ProtocolType, R: ReplicateSafe<P>> MutWrapper<'a, P, R> {
     }
 }
 
-impl<'a, P: ProtocolType, R: ReplicateSafe<P>> ReplicaRefTrait<P, R> for MutWrapper<'a, P, R> {
+impl<'a, P: ProtocolType, R: ReplicateSafe<P>> ReplicaRefTrait<P, R> for ComponentMut<'a, P, R> {
     fn to_ref(&self) -> &R {
         return &self.inner;
     }
 }
 
-impl<'a, P: ProtocolType, R: ReplicateSafe<P>> ReplicaMutTrait<P, R> for MutWrapper<'a, P, R> {
+impl<'a, P: ProtocolType, R: ReplicateSafe<P>> ReplicaMutTrait<P, R> for ComponentMut<'a, P, R> {
     fn to_mut(&mut self) -> &mut R {
         return &mut self.inner;
     }
