8dca7af7297f6de83ce2bc80c9e99b84abbb0016
Connor Carpenter <connorcarpenter@gmail.com>
2021-11-02 16:50:23 -0600

    No need to store local entities/component keys in server transaction records when we can derive that from their global counterparts

--- server/src/entity_action.rs
+++ server/src/entity_action.rs
@@ -1,32 +1,28 @@
-use naia_shared::{DiffMask, EntityActionType, LocalComponentKey, LocalEntity, ProtocolType};
+use naia_shared::{DiffMask, EntityActionType, ProtocolType};
 
 use super::keys::ComponentKey;
 
 #[derive(Clone, Debug)]
 pub enum EntityAction<P: ProtocolType, E: Copy> {
-    SpawnEntity(
-        E,
-        LocalEntity,
-        Vec<(ComponentKey, LocalComponentKey, P::Kind)>,
-    ),
-    DespawnEntity(E, LocalEntity),
-    OwnEntity(E, LocalEntity),
-    DisownEntity(E, LocalEntity),
-    InsertComponent(E, LocalEntity, ComponentKey, LocalComponentKey, P::Kind),
-    UpdateComponent(E, ComponentKey, LocalComponentKey, DiffMask, P::Kind),
-    RemoveComponent(ComponentKey, LocalComponentKey),
+    SpawnEntity(E, Vec<(ComponentKey, P::Kind)>),
+    DespawnEntity(E),
+    OwnEntity(E),
+    DisownEntity(E),
+    InsertComponent(E, ComponentKey, P::Kind),
+    UpdateComponent(E, ComponentKey, DiffMask, P::Kind),
+    RemoveComponent(ComponentKey),
 }
 
 impl<P: ProtocolType, E: Copy> EntityAction<P, E> {
     pub fn as_type(&self) -> EntityActionType {
         match self {
-            EntityAction::SpawnEntity(_, _, _) => EntityActionType::SpawnEntity,
-            EntityAction::DespawnEntity(_, _) => EntityActionType::DespawnEntity,
-            EntityAction::OwnEntity(_, _) => EntityActionType::OwnEntity,
-            EntityAction::DisownEntity(_, _) => EntityActionType::DisownEntity,
-            EntityAction::InsertComponent(_, _, _, _, _) => EntityActionType::InsertComponent,
-            EntityAction::UpdateComponent(_, _, _, _, _) => EntityActionType::UpdateComponent,
-            EntityAction::RemoveComponent(_, _) => EntityActionType::RemoveComponent,
+            EntityAction::SpawnEntity(_, _) => EntityActionType::SpawnEntity,
+            EntityAction::DespawnEntity(_) => EntityActionType::DespawnEntity,
+            EntityAction::OwnEntity(_) => EntityActionType::OwnEntity,
+            EntityAction::DisownEntity(_) => EntityActionType::DisownEntity,
+            EntityAction::InsertComponent(_, _, _) => EntityActionType::InsertComponent,
+            EntityAction::UpdateComponent(_, _, _, _) => EntityActionType::UpdateComponent,
+            EntityAction::RemoveComponent(_) => EntityActionType::RemoveComponent,
         }
     }
 }
