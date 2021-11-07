d76036a2730c92d84f0c6c9d1a86ee48a2c1498c
Connor Carpenter <naia.rust.crate@gmail.com>
2021-11-07 11:20:13 -0700

    - separate out User & UserRecord

--- adapters/bevy/server/src/events.rs
+++ adapters/bevy/server/src/events.rs
@@ -1,9 +1,9 @@
 use bevy::ecs::entity::Entity;
 
-use naia_server::{ProtocolType, User, UserKey};
+use naia_server::{ProtocolType, UserRecord, UserKey};
 
 pub struct AuthorizationEvent<P: ProtocolType>(pub UserKey, pub P);
 pub struct ConnectionEvent(pub UserKey);
-pub struct DisconnectionEvent(pub UserKey, pub User);
+pub struct DisconnectionEvent(pub UserKey, pub UserRecord);
 pub struct MessageEvent<P: ProtocolType>(pub UserKey, pub P);
 pub struct CommandEvent<P: ProtocolType>(pub UserKey, pub Entity, pub P);
--- server/src/user.rs
+++ server/src/user.rs
@@ -1,6 +1,10 @@
 use std::{collections::HashSet, hash::Hash, net::SocketAddr};
 
-use naia_shared::Timestamp;
+use naia_shared::{Timestamp, ProtocolType};
+
+use crate::{RoomKey, Server, UserKey};
+
+// UserKey
 
 #[allow(missing_docs)]
 #[allow(unused_doc_comments)]
@@ -9,28 +13,39 @@ pub mod user_key {
     new_key_type! { pub struct UserKey; }
 }
 
+// User
+
 #[derive(Clone)]
-pub struct User<E: Copy + Eq + Hash> {
+pub struct User {
     pub address: SocketAddr,
+}
+
+impl User {
+    pub fn new(address: SocketAddr) -> Self {
+        Self {
+            address,
+        }
+    }
+}
+
+// UserRecord
+
+#[derive(Clone)]
+pub struct UserRecord<E: Copy + Eq + Hash> {
+    pub user: User,
     pub timestamp: Timestamp,
     pub owned_entities: HashSet<E>,
 }
 
-impl<E: Copy + Eq + Hash> User<E> {
-    pub fn new(address: SocketAddr, timestamp: Timestamp) -> User<E> {
-        User {
-            address,
+impl<E: Copy + Eq + Hash> UserRecord<E> {
+    pub fn new(address: SocketAddr, timestamp: Timestamp) -> UserRecord<E> {
+        UserRecord {
+            user: User::new(address),
             timestamp,
             owned_entities: HashSet::new(),
         }
     }
 }
-
-// user references
-
-use naia_shared::ProtocolType;
-
-use crate::{RoomKey, Server, UserKey};
 
 // UserRef
 
