eb6682ae884988817ceb38b10d8a9f37e3ae121d
Connor Carpenter <connorcarpenter@gmail.com>
2022-02-09 22:02:34 -0600

    remove dev_http_server & webpack_dev_server and use cargo-make instead

--- demos/tickless/client/src/main.rs
+++ demos/tickless/client/src/main.rs
@@ -1,21 +1,33 @@
-extern crate cfg_if;
-
 #[macro_use]
+extern crate cfg_if;
 extern crate log;
 
-use log::LevelFilter;
-use simple_logger::SimpleLogger;
-
 mod app;
-mod loop_native;
+mod app_loop;
 
-fn main() {
-    // Uncomment the line below to enable logging. You don't need it if something
-    // else (e.g. quicksilver) is logging for you
-    SimpleLogger::new()
-        .with_level(LevelFilter::Info)
-        .init()
-        .expect("A logger was already initialized");
+use app::App;
+use app_loop::start_loop;
 
-    loop_native::start_loop(&mut app::App::new());
+cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+
+        fn main() {
+            // Uncomment the line below to enable logging. You don't need it if something else (e.g. quicksilver) is logging for you
+            wasm_logger::init(wasm_logger::Config::default());
+
+            start_loop(App::new());
+        }
+    } else {
+
+        fn main() {
+            // Uncomment the line below to enable logging. You don't need it if something
+            // else (e.g. quicksilver) is logging for you
+            simple_logger::SimpleLogger::new()
+                .with_level(log::LevelFilter::Info)
+                .init()
+                .expect("A logger was already initialized");
+
+            start_loop(App::new());
+        }
+    }
 }
