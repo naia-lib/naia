fd1b34b35b077500a5526105b2352c8b54287007
Connor Carpenter <naia.rust.crate@gmail.com>
2022-02-18 15:55:21 -0600

    Property only mutates if inner value changes

--- shared/src/property.rs
+++ shared/src/property.rs
@@ -24,6 +24,52 @@ impl<T: Clone + DeBin + SerBin + PartialEq> Property<T> {
         };
     }
 
+    // Getter / setter
+
+    /// Gets a reference to the value contained by the Property
+    pub fn get(&self) -> &T {
+        return &self.inner;
+    }
+
+    /// Gets a mutable reference to the value contained by the Property, queues
+    /// to update
+    pub fn get_mut(&mut self) -> &mut T {
+        // Just assume inner value will be changed, queue for update
+        if let Some(mutator) = &mut self.mutator {
+            mutator.mutate(self.mutator_index);
+        }
+        return &mut self.inner;
+    }
+
+    /// Set the Property's contained value, queues for update if value changes
+    pub fn set(&mut self, value: T) {
+        if self.inner != value {
+            if let Some(mutator) = &mut self.mutator {
+                mutator.mutate(self.mutator_index);
+            }
+            self.inner = value;
+        }
+    }
+
+    /// Set value to the value of another Property, queues for update if value changes
+    pub fn mirror(&mut self, other: &Property<T>) {
+        self.set(other.inner.clone());
+    }
+
+    // Serialization / deserialization
+
+    /// Writes contained value into outgoing byte stream
+    pub fn write(&self, buffer: &mut Vec<u8>) {
+        let encoded = &mut SerBin::serialize_bin(&self.inner);
+        buffer.push(encoded.len() as u8);
+        buffer.append(encoded);
+    }
+
+    /// Returns the number of bytes used to encode / decode the Property
+    pub fn size() -> usize {
+        std::mem::size_of::<T>() + 1
+    }
+
     /// Given a cursor into incoming packet data, initializes the Property with
     /// the synced value
     pub fn new_read(reader: &mut PacketReader, mutator_index: u8) -> Self {
@@ -34,11 +80,6 @@ impl<T: Clone + DeBin + SerBin + PartialEq> Property<T> {
             mutator: None,
             mutator_index,
         };
-    }
-
-    /// Returns the number of bytes used to encode / decode the Property
-    pub fn size() -> usize {
-        std::mem::size_of::<T>() + 1
     }
 
     /// Given a cursor into incoming packet data, updates the Property with the
@@ -64,44 +105,14 @@ impl<T: Clone + DeBin + SerBin + PartialEq> Property<T> {
         return inner;
     }
 
-    /// Gets a mutable reference to the value contained by the Property, queue
-    /// to update
-    pub fn get_mut(&mut self) -> &mut T {
-        if let Some(mutator) = &mut self.mutator {
-            mutator.mutate(self.mutator_index);
-        }
-        return &mut self.inner;
-    }
-
-    /// Set the Property's contained value
-    pub fn set(&mut self, value: T) {
-        if let Some(mutator) = &mut self.mutator {
-            mutator.mutate(self.mutator_index);
-        }
-        self.inner = value;
-    }
-
-    /// Gets a reference to the value contained by the Property
-    pub fn get(&self) -> &T {
-        return &self.inner;
-    }
+    // Comparison
 
     /// Compare to another property
     pub fn equals(&self, other: &Property<T>) -> bool {
         return self.inner == other.inner;
     }
 
-    /// Set value to the value of another Property
-    pub fn mirror(&mut self, other: &Property<T>) {
-        self.inner = other.inner.clone();
-    }
-
-    /// Writes contained value into outgoing byte stream
-    pub fn write(&self, buffer: &mut Vec<u8>) {
-        let encoded = &mut SerBin::serialize_bin(&self.inner);
-        buffer.push(encoded.len() as u8);
-        buffer.append(encoded);
-    }
+    // Internal
 
     /// Set an PropertyMutator to track changes to the Property
     pub fn set_mutator(&mut self, mutator: &PropertyMutator) {
