12caba2b93a3d2cfd68fc965fe2dbe6e7627ebd4
Connor Carpenter <connorcarpenter@gmail.com>
2022-03-01 17:24:24 -0600

    compress net entity serialization

--- shared/src/keys.rs
+++ shared/src/keys.rs
@@ -1,11 +1,11 @@
-use crate::{derive_serde, serde};
-
 // Local Entity
+
+use naia_serde::{BitReader, BitWrite, SerdeErr, UnsignedVariableInteger};
+use crate::{derive_serde, serde};
 
 // An Entity in the Client's scope, that is being
 // synced to the Client
-#[derive(Copy, Eq, Hash)]
-#[derive_serde]
+#[derive(Copy, Eq, Hash, Clone, PartialEq)]
 pub struct NetEntity(u16);
 
 impl From<u16> for NetEntity {
@@ -17,6 +17,17 @@ impl From<u16> for NetEntity {
 impl Into<u16> for NetEntity {
     fn into(self) -> u16 {
         self.0
+    }
+}
+
+impl serde::Serde for NetEntity {
+    fn ser<S: BitWrite>(&self, writer: &mut S) {
+        UnsignedVariableInteger::<7>::new(self.0).ser(writer);
+    }
+
+    fn de(reader: &mut BitReader) -> Result<Self, SerdeErr> {
+        let value = UnsignedVariableInteger::<7>::de(reader).unwrap().get();
+        return Ok(NetEntity(value as u16));
     }
 }
 
