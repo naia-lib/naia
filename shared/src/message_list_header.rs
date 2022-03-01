98fb898129b9fe4883c4e67f32ccedcb161648e0
Connor Carpenter <connorcarpenter@gmail.com>
2022-03-01 15:40:02 -0600

    send message count as smaller variable integer

--- shared/src/message_list_header.rs
+++ shared/src/message_list_header.rs
@@ -1,12 +1,20 @@
-use naia_serde::{BitReader, BitWrite, Serde};
+use naia_serde::{BitReader, BitWrite, Serde, UnsignedVariableInteger};
 
 pub fn write_list_header<S: BitWrite>(writer: &mut S, message_count: &u16) {
-    let has_messages: bool = *message_count > 0;
+    let mut message_count: u16 = *message_count;
+
+    let has_messages: bool = message_count > 0;
     has_messages.ser(writer);
 
     // write number of messages
     if has_messages {
-        message_count.ser(writer);
+
+        // we already know messages isn't 0, so you can send the count as a value >= 1
+        message_count -= 1;
+
+        let serde_count = UnsignedVariableInteger::<3>::new(message_count);
+
+        serde_count.ser(writer);
     }
 }
 
@@ -14,7 +22,14 @@ pub fn read_list_header(reader: &mut BitReader) -> u16 {
     let has_messages = bool::de(reader).unwrap();
 
     if has_messages {
-        return u16::de(reader).unwrap();
+        let serde_count = UnsignedVariableInteger::<3>::de(reader).unwrap();
+
+        let mut message_count = serde_count.get() as u16;
+
+        // we already know messages isn't 0, so you can send the count as a value >= 1
+        message_count += 1;
+
+        return message_count;
     } else {
         return 0;
     }
