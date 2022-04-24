53858ff4d0e737a5d92b9a0c0d83fc56be533460
Connor Carpenter <connorcarpenter@gmail.com>
2022-04-24 15:05:54 -0500

    - Derive Default on some types

--- adapters/bevy/client/src/resource.rs
+++ adapters/bevy/client/src/resource.rs
@@ -2,18 +2,9 @@ use std::default::Default;
 
 use naia_bevy_shared::Flag;
 
+#[derive(Default)]
 pub struct ClientResource {
     pub ticker: Flag,
     pub connector: Flag,
     pub disconnector: Flag,
-}
-
-impl Default for ClientResource {
-    fn default() -> Self {
-        Self {
-            ticker: Flag::new(),
-            connector: Flag::new(),
-            disconnector: Flag::new(),
-        }
-    }
 }
