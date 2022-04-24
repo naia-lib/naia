a945c06b4cf0c29a223e7a4a801fb51bff254910
Connor Carpenter <connorcarpenter@gmail.com>
2022-04-23 20:19:55 -0500

    - Updating all bevy adapter crates to make cargo happy

--- adapters/bevy/client/src/resource.rs
+++ adapters/bevy/client/src/resource.rs
@@ -2,9 +2,18 @@ use std::default::Default;
 
 use naia_bevy_shared::Flag;
 
-#[derive(Default)]
 pub struct ClientResource {
     pub ticker: Flag,
     pub connector: Flag,
     pub disconnector: Flag,
+}
+
+impl Default for ClientResource {
+    fn default() -> Self {
+        Self {
+            ticker: Flag::new(),
+            connector: Flag::new(),
+            disconnector: Flag::new(),
+        }
+    }
 }
