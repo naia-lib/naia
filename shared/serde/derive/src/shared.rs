240a6d20f1961ccafd091fda8470a7cef89f1d3d
Connor Carpenter <connorcarpenter@gmail.com>
2022-08-13 17:32:51 -0400

    - ignore clippy warning

--- shared/serde/derive/src/shared.rs
+++ shared/serde/derive/src/shared.rs
@@ -1,11 +1,12 @@
 
-#[allow(clippy::format_push_string)]
 macro_rules! l {
     ($target:ident, $line:expr) => {
+        #[allow(clippy::format_push_string)]
         $target.push_str($line);
     };
 
     ($target:ident, $line:expr, $($param:expr),*) => {
+        #[allow(clippy::format_push_string)]
         $target.push_str(&format!($line, $($param,)*));
     };
 }
