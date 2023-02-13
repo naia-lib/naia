2518c6d60177f813cff7b6b31ec40bc7a28c7e25
Connor Carpenter <connorcarpenter@gmail.com>
2023-02-13 15:41:26 -0600

    - fix Hecs demo - fix Bevy Wasm demo

--- demos/hecs/client/src/systems/events.rs
+++ demos/hecs/client/src/systems/events.rs
@@ -15,22 +15,39 @@ pub fn process_events(app: &mut App) {
 
     let mut events = app.client.receive(&mut app.world);
 
+    // Connect Events
     for server_address in events.read::<ConnectEvent>() {
         info!("Client connected to: {}", server_address);
     }
+
+    // Disconnect Events
     for server_address in events.read::<DisconnectEvent>() {
         info!("Client disconnected from: {}", server_address);
     }
+
+    // Spawn Events
     for entity in events.read::<SpawnEntityEvent>() {
         let new_id = app.next_id;
         app.next_id = app.next_id.wrapping_add(1);
         app.entity_to_id_map.insert(entity, new_id);
         info!("creation of entity: {new_id}");
     }
-    for (entity, _) in events.read::<InsertComponentEvent>() {
+
+    // Insert Component Events
+    for entity in events.read::<InsertComponentEvent<Marker>>() {
         let id = app.entity_to_id_map.get(&entity).unwrap();
-        info!("insert component into entity: {id}");
+        info!("insert Marker component into entity: {id}");
     }
+    for entity in events.read::<InsertComponentEvent<Name>>() {
+        let id = app.entity_to_id_map.get(&entity).unwrap();
+        info!("insert Name component into entity: {id}");
+    }
+    for entity in events.read::<InsertComponentEvent<Position>>() {
+        let id = app.entity_to_id_map.get(&entity).unwrap();
+        info!("insert Position component into entity: {id}");
+    }
+
+    // Remove Component Events
     for (entity, _) in events.read::<RemoveComponentEvent<Marker>>() {
         let id = app.entity_to_id_map.get(&entity).unwrap();
         info!("remove Marker component from entity: {id}");
@@ -43,13 +60,19 @@ pub fn process_events(app: &mut App) {
         let id = app.entity_to_id_map.get(&entity).unwrap();
         info!("remove Position component from entity: {id}");
     }
+
+    // Despawn Events
     for entity in events.read::<DespawnEntityEvent>() {
         let id = app.entity_to_id_map.remove(&entity).unwrap();
         info!("deletion of entity: {id}");
     }
+
+    // Tick Events
     for _ in events.read::<TickEvent>() {
         app.tick();
     }
+
+    // Error Events
     for error in events.read::<ErrorEvent>() {
         info!("Naia Client Error: {}", error);
     }
