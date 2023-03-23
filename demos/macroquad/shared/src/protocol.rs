e00566f16725b3b0895c4810ee0bf0700b5b0b8c
Connor Carpenter <connorcarpenter@gmail.com>
2023-03-22 21:45:36 -0500

    update macroquad & bevy demos with a more average tick rate of 25/second

--- demos/macroquad/shared/src/protocol.rs
+++ demos/macroquad/shared/src/protocol.rs
@@ -8,8 +8,8 @@ use crate::{channels::ChannelsPlugin, components::ComponentsPlugin, messages::Me
 pub fn protocol() -> Protocol {
     Protocol::builder()
         // Config
-        .tick_interval(Duration::from_millis(250))
-        .link_condition(LinkConditionerConfig::new(100, 0, 0.0))
+        .tick_interval(Duration::from_millis(40))
+        .link_condition(LinkConditionerConfig::good_condition())
         .enable_client_authoritative_entities()
         // Channels
         .add_plugin(ChannelsPlugin)
