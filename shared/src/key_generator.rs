4070727125add8fee3c70e547c66243d45a33d56
Connor Carpenter <connorcarpenter@gmail.com>
2023-04-27 15:59:25 -0500

    fix bug with Entity Waitlist Handle recycling

--- shared/src/key_generator.rs
+++ shared/src/key_generator.rs
@@ -4,9 +4,10 @@ use naia_socket_shared::Instant;
 
 /// Simple implementation of a store that manages a recycling pool of u16 keys
 pub struct KeyGenerator<K: From<u16> + Into<u16> + Copy> {
-    recycled_local_keys: VecDeque<(u16, Instant)>,
+    recycling_keys: VecDeque<(u16, Instant)>,
+    recycled_keys: VecDeque<u16>,
     recycle_timeout: Duration,
-    next_new_local_key: u16,
+    next_new_key: u16,
     phantom: PhantomData<K>,
 }
 
@@ -14,37 +15,42 @@ impl<K: From<u16> + Into<u16> + Copy> KeyGenerator<K> {
     pub fn new(recycle_timeout: Duration) -> Self {
         Self {
             recycle_timeout,
-            recycled_local_keys: VecDeque::new(),
-            next_new_local_key: 0,
+            recycling_keys: VecDeque::new(),
+            recycled_keys: VecDeque::new(),
+            next_new_key: 0,
             phantom: PhantomData,
         }
     }
     /// Get a new, unused key
     pub fn generate(&mut self) -> K {
+        // Check whether we can recycle any keys
         loop {
-            let Some((_, instant)) = self.recycled_local_keys.front() else {
+            let Some((_, instant)) = self.recycling_keys.front() else {
                 break;
             };
             if instant.elapsed() < self.recycle_timeout {
                 break;
             }
-            self.recycled_local_keys.pop_front();
+            let (key, _) = self.recycling_keys.pop_front().unwrap();
+            self.recycled_keys.push_back(key);
         }
 
-        if self.recycled_local_keys.len() > 0 {
-            let (local_key, _) = self.recycled_local_keys.pop_front().unwrap();
-            return K::from(local_key);
+        // Check whether we can return a recycled key
+        if self.recycled_keys.len() > 0 {
+            let key = self.recycled_keys.pop_front().unwrap();
+            return K::from(key);
         }
 
-        let output = self.next_new_local_key;
-        self.next_new_local_key = self.next_new_local_key.wrapping_add(1);
+        // Create a new key
+        let output = self.next_new_key;
+        self.next_new_key = self.next_new_key.wrapping_add(1);
         K::from(output)
     }
 
     /// Recycle a used key, freeing it up
-    pub fn recycle_key(&mut self, local_key: &K) {
-        let local_key_u16: u16 = Into::<u16>::into(*local_key);
-        self.recycled_local_keys
-            .push_back((local_key_u16, Instant::now()));
+    pub fn recycle_key(&mut self, key: &K) {
+        let key_u16: u16 = Into::<u16>::into(*key);
+        self.recycling_keys
+            .push_back((key_u16, Instant::now()));
     }
 }
