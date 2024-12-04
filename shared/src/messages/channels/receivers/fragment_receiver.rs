d94d2d9389a0cacae911b3dfa33a9c897c0387a6
Connor Carpenter <connorcarpenter@gmail.com>
2024-12-04 13:56:34 -0700

    - refactored logic behind ordered reliable channels, which was not handling fragments correctly, and discarding key information used to ensure correct ordering

--- shared/src/messages/channels/receivers/fragment_receiver.rs
+++ shared/src/messages/channels/receivers/fragment_receiver.rs
@@ -2,13 +2,11 @@ use std::collections::HashMap;
 
 use naia_serde::BitReader;
 
-use crate::{
-    messages::fragment::{FragmentId, FragmentedMessage},
-    LocalEntityAndGlobalEntityConverter, MessageContainer, MessageKinds,
-};
+use crate::{messages::fragment::{FragmentIndex, FragmentId, FragmentedMessage}, LocalEntityAndGlobalEntityConverter, MessageContainer, MessageIndex, MessageKinds};
 
 pub struct FragmentReceiver {
-    map: HashMap<FragmentId, (u32, Vec<Box<[u8]>>)>,
+    // <FragmentId, (FragmentsReceived, Option(FirstMessageIndex, FragmentCount), FragmentData)
+    map: HashMap<FragmentId, (u32, Option<(MessageIndex, u32)>, Vec<Box<[u8]>>)>,
 }
 
 impl FragmentReceiver {
@@ -22,8 +20,9 @@ impl FragmentReceiver {
         &mut self,
         message_kinds: &MessageKinds,
         converter: &dyn LocalEntityAndGlobalEntityConverter,
+        message_index: MessageIndex,
         message: MessageContainer,
-    ) -> Option<MessageContainer> {
+    ) -> Option<(MessageIndex, MessageIndex, MessageContainer)> {
         if !message.is_fragment() {
             panic!("Received non-fragmented message in FragmentReceiver!");
         }
@@ -36,12 +35,20 @@ impl FragmentReceiver {
         let fragment_id = fragment.id();
         let fragment_index = fragment.index();
         let fragment_total = fragment.total().as_usize();
-        // info!("fragment_total: {fragment_total}");
+
         if !self.map.contains_key(&fragment_id) {
             self.map
-                .insert(fragment_id, (0, vec![Box::new([]); fragment_total]));
+                .insert(fragment_id, (0, None, vec![Box::new([]); fragment_total]));
         }
-        let (fragments_received, fragment_list) = self.map.get_mut(&fragment_id).unwrap();
+        let (fragments_received, first_message_id_opt, fragment_list) = self.map.get_mut(&fragment_id).unwrap();
+
+        if fragment_index == FragmentIndex::zero() {
+            if first_message_id_opt.is_some() {
+                panic!("Received first fragment of message that already has a first fragment!");
+            }
+            *first_message_id_opt = Some((message_index, fragment_total as u32));
+        }
+
         fragment_list[fragment_index.as_usize()] = fragment.to_payload();
         *fragments_received += 1;
         if *fragments_received != fragment_total as u32 {
@@ -49,7 +56,8 @@ impl FragmentReceiver {
         }
 
         // we have received all fragments! put it all together
-        let (_, fragment_list) = self.map.remove(&fragment_id).unwrap();
+        let (_, first_index_opt, fragment_list) = self.map.remove(&fragment_id).unwrap();
+        let (first_message_index, fragment_count) = first_index_opt.unwrap();
         let concat_list = fragment_list.concat();
         let mut reader = BitReader::new(&concat_list);
         let full_message_result = message_kinds.read(&mut reader, converter);
@@ -58,6 +66,7 @@ impl FragmentReceiver {
             panic!("Cannot read fragmented message!");
         }
         let full_message = full_message_result.unwrap();
-        Some(full_message)
+        let end_message_index = first_message_index + fragment_count as u16 - 1;
+        Some((first_message_index, end_message_index, full_message))
     }
 }
